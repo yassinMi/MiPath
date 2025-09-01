67f55cb2a1622f5be38baf1d25761b5be7ce7506
yassinMi <DIR16CAT17@gmail.com>
2025-09-01 17:02:22 +0100

    Added new logo

--- freelancerprojectmanager.client/src/App.tsx
+++ freelancerprojectmanager.client/src/App.tsx
@@ -52,7 +52,7 @@ function App() {
     return (
         <div style={{colorScheme:darkMode?"dark":"light"}} className={`app-main min-h-screen  scrollbar scrollbar-thumb-gray-300 scrollbar-track-gray-100
             dark:scrollbar-thumb-gray-700 dark:scrollbar-track-gray-800  flex flex-col text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-900 ${darkMode ? "dark" : "light"}`}>
-            <AppHeader onDarkToggle={onDarkToggle} isDark={darkMode} subtitle="Projects" title="Freelancer Project Manager"></AppHeader>
+            <AppHeader onDarkToggle={onDarkToggle} isDark={darkMode} subtitle="Projects" title="Path"></AppHeader>
               <Outlet></Outlet>
              
     
