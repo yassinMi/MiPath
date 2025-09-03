44b0138abf3a3bbe43fb892a2f835a063e5195c8
yassinMi <DIR16CAT17@gmail.com>
2025-09-04 00:51:24 +0100

    Backend: returning the guest user on invalid jwt

--- MiPath.Server/Api/Controllers/AccountController.cs
+++ MiPath.Server/Api/Controllers/AccountController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MiPath.Server.Application.UserManagement.Queries;
+using MiPath.Server.Application.UserManagement.Queries.Dto;
+using MiPath.Server.Domain.UserManagement;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace MiPath.Server.Api.Controllers
 {
@@ -9,13 +13,27 @@ namespace MiPath.Server.Api.Controllers
     public class AccountController : ControllerBase
     {
         [HttpGet("me")]
-        public IActionResult Me()
+        public async Task<IActionResult> Me([FromServices] GetUserByIdQueryHandler handler)
         {
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var email = User.FindFirstValue(ClaimTypes.Email);
             var name = User.FindFirstValue(ClaimTypes.Name);
+            UserDto? user = null;
+            if (int.TryParse(userId, out int useridInt_))
+            {
+                user = await handler.Handle(new GetUserByIdQuery() { UserID = useridInt_ }, CancellationToken.None);
+            }
+            
+            if(user == null)
+            {
+                return Ok(new { IsGuest = true });
+            }
+            else
+            {
+                return Ok(new { UserId = user.ID, Email = user.Email, Name = user.Name });
+            }
 
-            return Ok(new { userId, email, name });
+                
         }
     }
 }
